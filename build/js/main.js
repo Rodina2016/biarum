860be61f0d6b8af68bf6a909cf1c0feaa942770c
Masha <marusea_rod@list.ru>
2020-01-28 18:01:37 +0300

    add content-block

--- build/js/main.js
+++ build/js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('#banner').owlCarousel({
        items: 1,
     });
+
+    $('#slider').owlCarousel({
+        items: 3,
+        dots: false,
+        nav: true,
+        navText: [" ", " "],
+        navClass: ['cards-btn cards-btn--prev', 'cards-btn cards-btn--next'],
+    });
 });
