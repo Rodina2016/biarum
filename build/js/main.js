d4297e9f16cf4804a5ad06b17237cf7b71ed12f6
Marodina <marusea_rod@list.ru>
2020-01-31 00:59:31 +0300

    end

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
     $('#banner').owlCarousel({
-       items: 1,
+        items: 1,
     });
 
     $('#slider').owlCarousel({
-        items: 3,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 3
+            }
+        },
         dots: false,
         nav: true,
         navText: [" ", " "],
         navClass: ['cards-btn cards-btn--prev', 'cards-btn cards-btn--next'],
+    });
+
+    $('#burger').click(function () {
+        $('.header__menu').toggleClass('show');
+        $(this).toggleClass('show');
     });
 
     $('input').change(function () {
